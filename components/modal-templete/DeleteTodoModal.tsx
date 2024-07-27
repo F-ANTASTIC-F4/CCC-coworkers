ec102719c435810eab5c7016641da772e3917f25
WooGi1020 <jungwk1020@gmail.com>
2024-07-27 23:54:03 +0900

    ✨ feat: 할 일 삭제 모달 구현

---
+++ components/modal-templete/DeleteTodoModal.tsx
@@ -0,0 +1,41 @@
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import WarningIcon from '@/public/icons/modal/warning_icon.svg';
+import React from 'react';
+
+function DeleteTodoModal({ title, ...props }: { title: string }) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <button type="button">삭제하기</button>
+      </DialogTrigger>
+      <DialogContent>
+        <WarningIcon />
+        <DialogTitle className="mb-[-10px] flex flex-col gap-3 text-center">
+          <p>&apos;{title}&apos;</p>
+          <p>할 일을 정말 삭제하시겠어요?</p>
+        </DialogTitle>
+        <DialogDescription>삭제 후에는 되돌릴 수 없습니다.</DialogDescription>
+        <div className="gap- flex w-full max-w-[280px] gap-2">
+          <DialogClose asChild>
+            <Button variant="outlined-secondary">닫기</Button>
+          </DialogClose>
+          <DialogClose asChild>
+            <Button variant="danger" {...props}>
+              삭제
+            </Button>
+          </DialogClose>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default DeleteTodoModal;
