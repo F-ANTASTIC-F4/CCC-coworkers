03f28699774a1d69d1115c4f16b4c5aebe7eeda8
CitrusSoda <dndhkaus1004@naver.com>
2024-07-29 15:25:29 +0900

    💄 design: 랜딩 모바일 구현 (Jira Ticket : CCC-21)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
-import { LandingCard } from '@/components/landing/LandingCard';
+import {
+  LandingCard,
+  LandingContainer,
+} from '@/components/landing/LandingCard';
 import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import FirstLandingCardIcon from '@/public/icons/landing_card_first_icon.svg';
+import SecondLandingCardIcon from '@/public/icons/landing_card_second_icon.svg';
+import ThirdLandingCardIcon from '@/public/icons/landing_card_third_icon.svg';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -20,10 +28,73 @@ export default function Home() {
         </Button>
       </section>
       <section className="space-y-6 px-4 md:px-6 xl:mx-auto xl:w-[1000px] xl:space-y-20 xl:px-0">
-        <LandingCard>hello</LandingCard>
-        <LandingCard className="bg-background-secondary">Hi</LandingCard>
+        <LandingCard>
+          <LandingContainer>
+            <div className="mt-12 flex flex-col gap-y-4">
+              <div className="flex size-12 items-center justify-center rounded-xl border bg-background-secondary">
+                <FirstLandingCardIcon />
+              </div>
+              <Label className="text-lg font-medium">
+                그룹으로
+                <br /> 할 일을 관리해요
+              </Label>
+            </div>
+            <div className="absolute bottom-0 h-[273px] w-[235px] xl:h-[338px] xl:w-[291px]">
+              <Image
+                src="/images/landing_card_first.png"
+                alt="first landing card"
+                fill
+                sizes="(min-width: 768px) 291px, 235px"
+                className="object-contain"
+              />
+            </div>
+          </LandingContainer>
+        </LandingCard>
+        <LandingCard className="bg-background-secondary">
+          <LandingContainer>
+            <div className="absolute bottom-12 flex flex-col gap-y-4">
+              <div className="flex size-12 items-center justify-center rounded-xl border bg-background-secondary">
+                <SecondLandingCardIcon />
+              </div>
+              <Label className="text-lg font-medium">
+                간단하게 멤버들을
+                <br /> 초대해요
+              </Label>
+            </div>
+            <div className="absolute top-0 h-[273px] w-[235px] xl:h-[338px] xl:w-[291px]">
+              <Image
+                src="/images/landing_card_second.png"
+                alt="second landing card"
+                fill
+                sizes="(min-width: 768px) 291px, 235px"
+                className="object-contain"
+              />
+            </div>
+          </LandingContainer>
+        </LandingCard>
         {/* TODO - 화이트 모드 색상 고려할 것 */}
-        <LandingCard className="bg-[#020617]">bye</LandingCard>
+        <LandingCard className="bg-[#020617]">
+          <LandingContainer>
+            <div className="absolute bottom-12 flex flex-col gap-y-4">
+              <div className="flex size-12 items-center justify-center rounded-xl border bg-background-secondary">
+                <ThirdLandingCardIcon />
+              </div>
+              <Label className="text-lg font-medium">
+                할 일도 간편하게
+                <br /> 체크해요
+              </Label>
+            </div>
+            <div className="absolute top-0 h-[273px] w-[235px] xl:h-[338px] xl:w-[291px]">
+              <Image
+                src="/images/landing_card_third.png"
+                alt="third landing card"
+                fill
+                sizes="(min-width: 768px) 291px, 235px"
+                className="object-contain"
+              />
+            </div>
+          </LandingContainer>
+        </LandingCard>
       </section>
       <section className="h-screen bg-[url('/images/landing_bottom_small.png')] bg-cover bg-center bg-no-repeat md:bg-[url('/images/landing_bottom_medium.png')] xl:bg-[url('/images/landing_bottom_large.png')]">
         <div className="flex flex-col items-center justify-center pt-[123px] text-center md:pt-[176px] xl:pt-[230px]">
@@ -31,7 +102,8 @@ export default function Home() {
             지금 바로 시작해보세요
           </h1>
           <p className="text-base font-medium md:text-2xl">
-            팀원 모두와 같은 방향, 같은 속도로 나아가는 가장 쉬운 방법
+            팀원 모두와 같은 방향,
+            <br className="md:hidden" /> 같은 속도로 나아가는 가장 쉬운 방법
           </p>
         </div>
       </section>
--- components/landing/LandingCard.tsx
+++ components/landing/LandingCard.tsx
@@ -2,13 +2,14 @@ import cn from '@/lib/utils';
 import * as React from 'react';
 
 interface LandingCardProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface LandingContainerProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const LandingCard = React.forwardRef<HTMLDivElement, LandingCardProps>(
   ({ className, children, ...props }, ref) => (
     <div
       ref={ref}
       className={cn(
-        'h-[467px] rounded-xl border md:h-[354px] xl:h-[419px]',
+        'flex h-[467px] flex-col rounded-xl border md:h-[354px] md:flex-row md:justify-center xl:h-[419px]',
         className
       )}
       {...props}
@@ -19,5 +20,21 @@ const LandingCard = React.forwardRef<HTMLDivElement, LandingCardProps>(
 );
 LandingCard.displayName = 'LandingCard';
 
-// eslint-disable-next-line import/prefer-default-export
-export { LandingCard };
+const LandingContainer = React.forwardRef<
+  HTMLDivElement,
+  LandingContainerProps
+>(({ className, children, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      'relative mx-auto h-full w-[235px] md:mx-0 md:w-full',
+      className
+    )}
+    {...props}
+  >
+    {children}
+  </div>
+));
+LandingContainer.displayName = 'LandingContainer';
+
+export { LandingCard, LandingContainer };
