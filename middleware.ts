03ddce84487027d28f5d22440951baee53f8f2eb
eprnf <eprnf21@gmail.com>
2024-08-01 06:02:25 +0900

    ✨ feat: 비밀번호 재설정 관련 미들웨어 추가 (Jira Ticket : CCC-37)

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
 import validateTokenAndSetAuthHeader from '@/lib/middlewares/auth.middleware';
+import processTokenFromQuery from '@/lib/middlewares/reset-password.middleware';
 import { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
   const setAuthHeaderResponse = await validateTokenAndSetAuthHeader(request);
   if (setAuthHeaderResponse) return setAuthHeaderResponse;
+
+  const resetPasswordResponse = await processTokenFromQuery(request);
+  if (resetPasswordResponse) return resetPasswordResponse;
 }
