afba3a412d6f10ddc20d7c893e7c42a98e246ac1
WooGi1020 <jungwk1020@gmail.com>
2024-08-01 14:42:02 +0900

    ✨ feat: lint-disable 라인 추가 (Jira Ticket : CCC-33)

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -48,7 +48,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
+        // eslint-disable-next-line @typescript-eslint/no-shadow, react/no-unstable-nested-components, @typescript-eslint/no-unused-vars
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        // eslint-disable-next-line @typescript-eslint/no-shadow, react/no-unstable-nested-components, @typescript-eslint/no-unused-vars
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
