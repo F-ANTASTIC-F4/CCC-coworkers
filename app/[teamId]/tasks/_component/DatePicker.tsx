6eccc446e1cecdc7bacfe96b899186699291788a
WooGi1020 <jungwk1020@gmail.com>
2024-08-03 02:25:51 +0900

    ✨ feat: 달력 날짜 선택 로직 추가 (Jira Ticket : CCC-33)

--- app/[teamId]/tasks/_component/DatePicker.tsx
+++ app/[teamId]/tasks/_component/DatePicker.tsx
@@ -9,24 +9,42 @@ import {
 import CalenderButton from '@/public/icons/list/calender_icon.svg';
 import * as React from 'react';
 
-export default function DatePicker() {
+interface DatePickerProps {
+  onClick: (value: Date) => void;
+}
+
+function DatePicker({ onClick }: DatePickerProps) {
   const [date, setDate] = React.useState<Date>();
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+
+  const handleDate = (day: Date | undefined) => {
+    setDate(day);
+    setIsOpen(false);
+  };
+
+  React.useEffect(() => {
+    if (date) {
+      onClick(date);
+    }
+  }, [date, onClick]);
 
   return (
-    <Popover>
+    <Popover open={isOpen} onOpenChange={setIsOpen}>
       <PopoverTrigger asChild>
         <button type="button" aria-label="달력 표시 버튼">
           <CalenderButton />
         </button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
+      <PopoverContent className="w-auto rounded-3xl p-0">
         <Calendar
           mode="single"
           selected={date}
-          onSelect={setDate}
+          onSelect={handleDate}
           initialFocus
         />
       </PopoverContent>
     </Popover>
   );
 }
+
+export default React.memo(DatePicker);
