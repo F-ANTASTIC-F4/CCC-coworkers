c58457b4531c356edfb8334901ead8b9ac61b67a
eprnf <eprnf21@gmail.com>
2024-08-06 06:31:59 +0900

    ♻️ refactor: 폼 분리 및 스키마 파일 적용 (Jira Ticket : CCC-27)

--- app/(user)/my-account/page.tsx
+++ app/(user)/my-account/page.tsx
@@ -1,11 +1,17 @@
 import WithDrawalModal from '@/components/modal-template/WithDrawalModal';
 import fetchAPI from '@/lib/api/fetchAPI';
 import { deleteUser } from '@/lib/api/user';
+import ErrorFallbackUI from '@/lib/error-boundary/ErrorFallBackUI';
 
-import AccountEditForm from './AccountEditForm';
+import AccountEditForm from './_components/AccountEditForm';
 
 const Page = async () => {
-  const currentUserAccountInfo = await fetchAPI.User();
+  const { data: currentUserAccountInfo, error } = await fetchAPI.User();
+
+  // 서버사이드렌더링중 에러가 나는 부분은 직접 error component를 리턴
+  if (error) {
+    return <ErrorFallbackUI error={error} />;
+  }
 
   return (
     <div className="center mx-auto w-full max-w-[792px] flex-col gap-6 px-4">
