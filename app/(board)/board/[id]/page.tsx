a14beb6a4622effeb6580a86c364a5b3be7746c1
CitrusSoda <dndhkaus1004@naver.com>
2024-08-05 17:40:34 +0900

    💄 design: 댓글 달기 디자인만 구현 (Jira Ticket : CCC-39)

--- app/(board)/board/[id]/page.tsx
+++ app/(board)/board/[id]/page.tsx
@@ -7,6 +7,7 @@ function BoardDetail() {
     <div className="my-10 md:my-14">
       <BoardDetailDescription />
       <BoardCommentInput />
+      <hr className="my-8 md:my-10" />
       <BoardComments />
     </div>
   );
