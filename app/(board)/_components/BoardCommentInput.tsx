887cfc823889d4147204cf9b38c2a73b37e86430
CitrusSoda <dndhkaus1004@naver.com>
2024-08-05 18:02:00 +0900

    💄 design: 인풋 패딩 수정 (Jira Ticket : CCC-39)

--- app/(board)/_components/BoardCommentInput.tsx
+++ app/(board)/_components/BoardCommentInput.tsx
@@ -6,7 +6,7 @@ function BoardCommentInput() {
       <p className="font-medium md:text-xl md:font-bold">댓글달기</p>
       {/* TODO - react query 혹은 shadCN 폼 형식으로 변경할 것 */}
       <textarea
-        className="min-h-[104px] w-full resize-none rounded-xl border bg-background-secondary px-2 py-1 md:px-6 md:py-4"
+        className="min-h-[104px] w-full resize-none rounded-xl border bg-background-secondary px-4 py-2 md:px-6 md:py-4"
         placeholder="댓글을 입력해주세요."
       />
       <Button className="ml-auto h-[32px] w-[74px] text-sm md:h-12 md:w-[184px] md:text-base">
