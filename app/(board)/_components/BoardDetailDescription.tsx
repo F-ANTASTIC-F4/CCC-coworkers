03332fea5dc4baa6b793d32eeadfbfd55925fbac
CitrusSoda <dndhkaus1004@naver.com>
2024-08-05 17:19:49 +0900

    💄 design: 상세 페이지 폰트 추가 (Jira Ticket : CCC-39)

--- app/(board)/_components/BoardDetailDescription.tsx
+++ app/(board)/_components/BoardDetailDescription.tsx
@@ -8,18 +8,22 @@ function BoardDetailDescription() {
     <div className="flex flex-col gap-6 text-text-secondary">
       <div>
         <div className="mb-4 mt-6 flex justify-between">
-          <p>게시물 제목 영역입니다.</p>
+          <p className="font-medium md:text-lg">게시물 제목 영역입니다.</p>
           <KebabIcon className="size-6 cursor-pointer" />
         </div>
         <hr />
         <div className="mt-4 flex items-center justify-between">
           <div className="flex items-center gap-2 md:gap-4">
             <ProfileIcon className="size-8" />
-            <p className="text-text-primary">이름</p>
+            <p className="text-xs font-medium text-text-primary md:text-sm">
+              이름
+            </p>
             <div className="h-3 border-r border-slate-700" />
-            <p className="text-slate-400">nnnn. nn. nn</p>
+            <p className="text-xs font-medium text-slate-400 md:text-sm">
+              nnnn. nn. nn
+            </p>
           </div>
-          <div className="flex items-center gap-2 md:gap-4">
+          <div className="flex items-center gap-2 text-xs md:gap-4 md:text-sm">
             <div className="flex items-center gap-1">
               <CommentIcon className="size-4" />
               <p className="text-slate-400">n</p>
@@ -32,7 +36,7 @@ function BoardDetailDescription() {
         </div>
       </div>
       <div className="mt-6">
-        <p>
+        <p className="text-sm md:text-base">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis
           soluta aliquam quo magnam ipsam temporibus tempora cupiditate omnis?
           Fuga dolor iure repellat adipisci nihil veritatis excepturi doloribus
