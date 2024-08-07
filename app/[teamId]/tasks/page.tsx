b50a71d3a56dc40651e6c77db2853026af7c078e
WooGi1020 <jungwk1020@gmail.com>
2024-08-07 20:28:18 +0900

    ✨ feat: 데이터가 없을 경우 처리 (Jira Ticket : CCC-55)

--- app/[teamId]/tasks/page.tsx
+++ app/[teamId]/tasks/page.tsx
@@ -4,8 +4,8 @@ import TaskList from './_component/TaskList';
 
 function ListPage() {
   return (
-    <section className="relative mx-auto xl:max-w-[1232px]">
-      <div className="flex w-full flex-col gap-6 px-4">
+    <section className="min-screen relative mx-auto xl:max-w-[1232px]">
+      <div className="flex h-full w-full flex-col gap-6 px-4">
         <h1 className="mr-auto mt-6 text-[18px] font-bold text-text-primary">
           할 일
         </h1>
