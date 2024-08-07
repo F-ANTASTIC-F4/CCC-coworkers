4c328b4ece087ec79cb33e81fd0c614864061ada
eprnf <eprnf21@gmail.com>
2024-08-07 23:08:40 +0900

    🐛 fix: build 오류 수정 (Jira Ticket : CCC-56)

--- app/(user)/my-account/page.tsx
+++ app/(user)/my-account/page.tsx
@@ -9,13 +9,12 @@ import WithdrawalModal from './_components/WithdrawalModal';
 const Page = async () => {
   const { data: currentUserAccountInfo, error } = await fetchAPI.User();
 
-  // 유저 데이터가 없으면 랜딩페이지로 리다이렉트
-  if (isEmptyObject(currentUserAccountInfo)) redirect('/');
-
   // 서버사이드렌더링중 에러가 나는 부분은 직접 error component를 리턴
   if (error) {
     return <ErrorFallbackUI error={error} />;
   }
+  // 유저 데이터가 없으면 랜딩페이지로 리다이렉트
+  if (isEmptyObject(currentUserAccountInfo)) redirect('/');
 
   return (
     <div className="center mx-auto w-full max-w-[792px] flex-col gap-6 px-4">
