9bfb294edf6286750b00010221aa3271a7eb89d4
CitrusSoda <dndhkaus1004@naver.com>
2024-08-09 19:07:22 +0900

    ✨ feat: 할 일 목록 구현 (Jira Ticket : CCC-65)

--- app/[teamId]/page.tsx
+++ app/[teamId]/page.tsx
@@ -1,13 +1,16 @@
+import fetchAPI from '@/lib/api/fetchAPI';
+
 import TeamMember from './_components/TeamMember';
 import TeamReport from './_components/TeamReport';
 import TeamTitle from './_components/TeamTitle';
 import TeamToDoList from './_components/TeamToDoList';
 
-function TeamPage({ params }: { params: { teamId: string } }) {
+async function TeamPage({ params }: { params: { teamId: number } }) {
+  const { data } = await fetchAPI.Group(params.teamId);
   return (
     <div>
       <TeamTitle />
-      <TeamToDoList />
+      <TeamToDoList taskLists={data?.taskLists ?? []} />
       <TeamReport />
       <TeamMember />
       {/* TODO - param값 test */}
