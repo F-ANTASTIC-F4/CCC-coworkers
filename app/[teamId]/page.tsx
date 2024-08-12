6fda1bce16a5669f955512c4c1da2844a1de3aba
CitrusSoda <dndhkaus1004@naver.com>
2024-08-12 14:10:25 +0900

    ✨ feat: 멤버 구현 (Jira Ticket : CCC-65)

--- app/[teamId]/page.tsx
+++ app/[teamId]/page.tsx
@@ -7,14 +7,13 @@ import TeamToDoList from './_components/TeamToDoList';
 
 async function TeamPage({ params }: { params: { teamId: number } }) {
   const { data } = await fetchAPI.Group(params.teamId);
+
   return (
     <div>
       <TeamTitle />
       <TeamToDoList taskLists={data?.taskLists ?? []} />
       <TeamReport taskLists={data?.taskLists ?? []} />
-      <TeamMember />
-      {/* TODO - param값 test */}
-      <p>{params.teamId} 페이지입니다.</p>
+      <TeamMember members={data?.members ?? []} />
     </div>
   );
 }
