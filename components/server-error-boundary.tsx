0b387c961aee38a47788131a02e43a9d6d0d9288
eprnf <eprnf21@gmail.com>
2024-08-12 20:35:28 +0900

    ♻️ refactor: 서버에러바운더리 수정 (Jira Ticket : CCC-48)

--- components/server-error-boundary.tsx
+++ components/server-error-boundary.tsx
@@ -1,15 +1,9 @@
 'use client';
 
-import React, {
-  ErrorInfo,
-  createContext,
-  createElement,
-  isValidElement,
-  useContext,
-  useState,
-} from 'react';
-
 /* eslint-disable */
+import ErrorFallbackUI from '@/components/common/ErrorFallBackUI';
+import FetchError from '@/lib/api/HttpClient/FetchError';
+import React, { ErrorInfo, createContext, useContext, useState } from 'react';
 
 // 에러 바운더리의 상태 타입
 interface ErrorBoundaryState {
@@ -17,19 +11,16 @@ interface ErrorBoundaryState {
   didCatch: boolean;
   error: Error | null;
 }
-
 // 에러 바운더리의 props 타입
 interface ErrorBoundaryProps {
   children: React.ReactNode;
-  fallback?: React.ReactNode;
-  fallbackRender?: (props: { error: Error | null }) => React.ReactNode;
-  FallbackComponent?: React.ComponentType<{ error: Error | null }>;
+  fallback?: any;
   onError?: (error: Error, errorInfo: ErrorInfo) => void;
 }
 
 // 에러 바운더리의 컨텍스트 타입
 interface ErrorBoundaryContextType extends ErrorBoundaryState {
-  showBoundary: (error: Error) => void;
+  showBoundary?: (error: Error) => void;
 }
 
 const initialState: ErrorBoundaryState = {
@@ -47,22 +38,17 @@ export function useErrorBoundary() {
   if (!context) {
     throw new Error('반드시 ErrorBoundary 안에서 사용해야 합니다.');
   }
-
   const [state, setState] = useState(initialState);
-
   const showBoundary = (error: Error) =>
     setState({
       error,
       hasError: true,
       didCatch: true,
     });
-
   context.showBoundary = showBoundary;
-
   if (state.hasError) {
     throw state.error;
   }
-
   return context;
 }
 
@@ -75,55 +61,40 @@ class ServerErrorBoundary extends React.Component<
     this.state = initialState;
   }
 
-  static getDerivedStateFromError(error: Error) {
-    // getDerivedStateFromError, 생명주기 관련 함수
-    // 에러가 발생한 후 폴백 UI를 렌더링하는 데 사용
-    // 다음 렌더링에서 폴백 UI가 보이도록 상태를 업데이트 합니다.
+  static getDerivedStateFromError(error: Error | FetchError) {
+    // 하위 트리에서 throw된 error를 받습니다.
     return { hasError: true, didCatch: true, error };
   }
 
-  // componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-  //   // 생명주기 관련 함수
-  //   // 에러 정보를 기록하는데 사용
-  //   // 에러 리포팅 서비스에 에러를 기록할 수도 있습니다.
-  //   // logErrorToMyService(error, errorInfo);
-  //   this.props.onError?.(error, errorInfo);
-  // }
+  componentDidCatch(error: Error, info: ErrorInfo) {
+    // 오류나면 콘솔에 기록
+    console.log('---------------  오류가 발생했습니다   ---------------');
+    console.log(error, info);
+  }
 
   render() {
-    const { children, fallbackRender, FallbackComponent, fallback } =
-      this.props;
+    let childToRender;
 
-    const { hasError, didCatch, error } = this.state;
+    if (!this.state.hasError) {
+      childToRender = this.props.children;
+    }
 
-    let childToRender = children;
-
-    if (didCatch) {
+    //hasError값이 true로 바뀌면 fallback을 보여줍니다
+    if (this.state.hasError) {
       const props = {
-        error,
+        error: this.state.error,
       };
 
-      if (typeof fallbackRender === 'function') {
-        childToRender = fallbackRender(props);
-      } else if (FallbackComponent) {
-        childToRender = createElement(FallbackComponent, props);
-      } else if (fallback === null || isValidElement(fallback)) {
-        childToRender = fallback;
-      } else {
-        if (process.env.NODE_ENV !== 'production') {
-          console.error(
-            'react-error-boundary requires either a fallback, fallbackRender, or FallbackComponent prop'
-          );
-        }
-
-        throw error;
-      }
+      childToRender = (
+        <ErrorFallbackUI
+          {...props}
+          onClickRetry={() => this.setState(initialState)}
+        />
+      );
     }
 
     return (
-      <ErrorBoundaryContext.Provider
-        value={{ ...this.state, showBoundary: () => {} }}
-      >
+      <ErrorBoundaryContext.Provider value={this.state}>
         {childToRender}
       </ErrorBoundaryContext.Provider>
     );
