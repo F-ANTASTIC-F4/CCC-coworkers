1d9743a3dd975a212850021cae989acb0ceaec00
kimhyesun <hyesun1004.kim@gmail.com>
2024-08-15 05:29:14 +0900

    📦 chore: 로딩 이미지 (Jira Ticket : CCC-71)

---
+++ app/(auth)/oauth/loading.tsx
@@ -0,0 +1,11 @@
+import LoadingBright from '@/public/imagesloading_bright.svg';
+import LoadingShadow from '@/public/imagesloading_shadow.svg';
+
+export default function Loading() {
+  return (
+    <div>
+      <LoadingBright />
+      <LoadingShadow />
+    </div>
+  );
+}
