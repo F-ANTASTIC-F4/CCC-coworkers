caa24c06a926f2a34c53a85aa70d00a85bf06b25
CitrusSoda <dndhkaus1004@naver.com>
2024-08-19 21:54:13 +0900

    🐛 fix: es-toolkit을 이용한 debounce 설정 (Jira Ticket : CCC-74)

--- app/[groupId]/_components/TeamToDoList.tsx
+++ app/[groupId]/_components/TeamToDoList.tsx
@@ -6,13 +6,14 @@ import { GroupTask } from '@ccc-types';
 import {
   DndContext,
   DragEndEvent,
-  MouseSensor,
+  PointerSensor,
   useSensor,
   useSensors,
 } from '@dnd-kit/core';
 import { restrictToVerticalAxis } from '@dnd-kit/modifiers';
 import { SortableContext, arrayMove } from '@dnd-kit/sortable';
-import { useEffect, useState } from 'react';
+import { debounce } from 'es-toolkit';
+import { useState } from 'react';
 
 import TeamToDoListCard from './TeamToDoListCard';
 
@@ -26,37 +27,41 @@ function TeamToDoList({
   // 드래그앤 드랍을 위해 state 추가
   const [taskListsData, setTaskListsData] = useState(taskLists);
 
-  useEffect(() => {
-    // 데이터가 일치하는 않는다는 콘솔 오류가 떠서 추가
-    if (JSON.stringify(taskLists) !== JSON.stringify(taskListsData)) {
-      setTaskListsData(taskLists);
-    }
-  }, [taskLists, taskListsData]);
-
   // 마우스가 30픽셀 이상 이동해야 드래그 시작, 없으면 링크가 작동되며 수정 삭제가 안됨
   const sensors = useSensors(
-    useSensor(MouseSensor, {
+    useSensor(PointerSensor, {
       activationConstraint: {
         distance: 30,
       },
     })
   );
 
+  // 디바운스
+  const debouncedReorderTaskList = debounce(
+    async (taskListId: number, displayIndex: number) => {
+      await reorderTaskList(groupId, taskListId, {
+        displayIndex,
+      });
+    },
+    1000
+  );
+
   // 드래그앤 드랍 로직, 전 인덱스와 바꿀 인덱스를 바꿔주며 서버에 요청
-  const handleDragEnd = (event: DragEndEvent) => {
+  const handleDragEnd = async (event: DragEndEvent) => {
     const { active, over } = event;
 
     if (over && active.id !== over.id) {
-      setTaskListsData((prev) => {
-        const oldIndex = prev.findIndex(
-          (taskList) => taskList.id === active.id
-        );
-        const newIndex = prev.findIndex((taskList) => taskList.id === over.id);
-        reorderTaskList(groupId, active.id as number, {
-          displayIndex: newIndex,
-        });
-        return arrayMove(prev, oldIndex, newIndex);
-      });
+      const oldIndex = taskListsData.findIndex(
+        (taskList) => taskList.id === active.id
+      );
+
+      const newIndex = taskListsData.findIndex(
+        (taskList) => taskList.id === over.id
+      );
+
+      setTaskListsData((prev) => arrayMove(prev, oldIndex, newIndex));
+
+      debouncedReorderTaskList(active.id as number, newIndex);
     }
   };
 
