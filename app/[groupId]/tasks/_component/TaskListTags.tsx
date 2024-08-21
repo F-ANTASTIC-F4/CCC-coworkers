a7117d874a2a159f36c337b6b29ec6c814618798
WooGi1020 <jungwk1020@gmail.com>
2024-08-21 14:46:28 +0900

    ♻️ refactor: TaskListTag 컴포넌트 위치 변경 (Jira Ticket : CCC-72)

--- app/[groupId]/tasks/_component/TaskListTags.tsx
+++ app/[groupId]/tasks/_component/TaskListTags.tsx
@@ -3,6 +3,7 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 
 /* eslint-disable jsx-a11y/click-events-have-key-events */
+import TodoListModal from '@/components/modal-template/TodoListModal';
 import fetchAPI from '@/lib/api/fetchAPI';
 import { GroupTask, Id } from '@ccc-types';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
@@ -12,13 +13,17 @@ function TaskListTags({ groupId }: { groupId: Id }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  const [tag, setTag] = useState<GroupTask[] | null>([]);
+  const [tag, setTag] = useState<Omit<GroupTask, 'tasks'>[]>([]);
 
   const fetchListData = async (value: Id) => {
     const res = await fetchAPI.Group(value);
     if (res.data) {
       setTag(res.data.taskLists);
     }
+  };
+
+  const setNewList = (newList: Omit<GroupTask, 'tasks'>) => {
+    setTag((prev) => [...prev, newList]);
   };
 
   const handleClick = (taskId: string) => {
@@ -32,22 +37,31 @@ function TaskListTags({ groupId }: { groupId: Id }) {
   }, [groupId]);
 
   return (
-    <ul className="task-list-scroll my-2 mb-[-20px] flex gap-3 overflow-x-auto">
-      {tag?.map((item) => (
-        <li
-          key={item.id}
-          onClick={() => {
-            handleClick(item.id.toString());
-          }}
-          className={`cursor-pointer whitespace-nowrap text-base font-medium text-text-default ${
-            item.id === Number(searchParams.get('task-list')) &&
-            'border-b-2 border-text-primary pb-[3px] text-text-primary'
-          }`}
-        >
-          {item.name}
-        </li>
-      ))}
-    </ul>
+    <nav className="relative flex items-center justify-between">
+      <ul className="task-list-scroll my-2 mb-[-20px] flex gap-3 max-xl:overflow-x-auto">
+        {tag?.map((item) => (
+          <li
+            key={item.id}
+            onClick={() => {
+              handleClick(item.id.toString());
+            }}
+            className={`cursor-pointer whitespace-nowrap text-base font-medium text-text-default ${
+              item.id === Number(searchParams.get('task-list')) &&
+              'border-b-2 border-text-primary pb-[3px] text-text-primary'
+            }`}
+          >
+            {item.name}
+          </li>
+        ))}
+      </ul>
+      <div className="absolute right-0 top-[-43px]">
+        <TodoListModal
+          handleList={setNewList}
+          groupId={groupId}
+          className="ml-auto"
+        />
+      </div>
+    </nav>
   );
 }
 export default TaskListTags;
--- app/[groupId]/tasks/page.tsx
+++ app/[groupId]/tasks/page.tsx
@@ -1,5 +1,4 @@
 import Loading from '@/components/common/loading';
-import TodoListModal from '@/components/modal-template/TodoListModal';
 import { DateString, Id } from '@ccc-types';
 import { Suspense } from 'react';
 
@@ -17,23 +16,20 @@ async function ListPage({
   const { groupId } = params;
 
   return (
-    <section className="min-screen relative">
-      <div className="flex h-full w-full flex-col gap-6">
-        <h1 className="mr-auto mt-6 text-[18px] font-bold text-text-primary">
-          할 일
-        </h1>
-        <div className="mb-[-5px] flex items-center">
-          <TaskDateController />
-          <TodoListModal groupId={groupId} className="ml-auto" />
-        </div>
-        {groupId && <TaskListTags groupId={groupId} />}
-        <Suspense
-          key={`${groupId}-${searchParams?.['task-list']}-${searchParams?.date}`}
-          fallback={<Loading />}
-        >
-          <TaskList groupId={groupId} searchParams={searchParams} />
-        </Suspense>
+    <section className="min-screen relative flex flex-col gap-6">
+      <h1 className="mr-auto mt-6 text-[18px] font-bold text-text-primary">
+        할 일
+      </h1>
+      <div className="mb-[-5px] flex items-center">
+        <TaskDateController />
       </div>
+      {groupId && <TaskListTags groupId={groupId} />}
+      <Suspense
+        key={`${groupId}-${searchParams?.['task-list']}-${searchParams?.date}`}
+        fallback={<Loading />}
+      >
+        <TaskList groupId={groupId} searchParams={searchParams} />
+      </Suspense>
     </section>
   );
 }
