d3e715008ca36e54f19fcb6b85ec7ddeb7495d16
eprnf <eprnf21@gmail.com>
2024-08-23 23:59:24 +0900

    ♻️ refactor: client 함수 변경에 따른 api 함수 수정 (Jira Ticket : CCC-93)

--- lib/api/comment.ts
+++ lib/api/comment.ts
@@ -2,10 +2,11 @@
 
 import ENDPOINTS from '@/lib/api/ENDPOINTS';
 import client from '@/lib/api/client/client';
+import { handleApiResponse } from '@/lib/api/utils';
 import { Id } from '@ccc-types';
 
 export async function postComment(taskId: Id, comment: string) {
-  const { data, error } = await client<Omit<Comment, 'user'>>(
+  const res = await client<Omit<Comment, 'user'>>(
     ENDPOINTS.COMMENT.ACTIONS(taskId),
     {
       method: 'post',
@@ -14,49 +15,25 @@ export async function postComment(taskId: Id, comment: string) {
       },
     }
   );
-  if (error) {
-    return {
-      error: {
-        info: '메세지 생성 중 에러가 발생했습니다.',
-        message: error.message,
-        ...error.cause,
-      },
-    };
-  }
-  return { data };
+
+  return handleApiResponse(res, '메세지 생성 중 에러가 발생했습니다.');
 }
 
 export async function updateComment(commentId: Id, comment: string) {
-  const { error } = await client<void>(ENDPOINTS.COMMENT.ACTIONS(commentId), {
+  const res = await client<void>(ENDPOINTS.COMMENT.ACTIONS(commentId), {
     method: 'patch',
     data: {
       content: comment,
     },
   });
-  if (error) {
-    return {
-      error: {
-        info: '메세지 수정 중 에러가 발생했습니다.',
-        message: error.message,
-        ...error.cause,
-      },
-    };
-  }
-  return { data: true };
+
+  return handleApiResponse(res, '메세지 수정 중 에러가 발생했습니다.');
 }
 
 export async function deleteComment(commentId: Id) {
-  const { error } = await client<void>(ENDPOINTS.COMMENT.ACTIONS(commentId), {
+  const res = await client<void>(ENDPOINTS.COMMENT.ACTIONS(commentId), {
     method: 'delete',
   });
-  if (error) {
-    return {
-      error: {
-        info: '메세지 삭제 중 에러가 발생했습니다.',
-        message: error.message,
-        ...error.cause,
-      },
-    };
-  }
-  return { data: true };
+
+  return handleApiResponse(res, '메세지 삭제 중 에러가 발생했습니다.');
 }
