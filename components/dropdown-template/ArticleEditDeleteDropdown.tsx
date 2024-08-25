dbfe1bb1df59c2c84ae4a0b74d948f08568935ee
WooGi1020 <jungwk1020@gmail.com>
2024-08-26 02:14:27 +0900

    ✨ feat: 필요 드롭다운 및 모달 추가 (Jira Ticket : CCC-94)

---
+++ components/dropdown-template/ArticleEditDeleteDropdown.tsx
@@ -0,0 +1,75 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import KebabIcon from '@/public/icons/kebab_icon.svg';
+import * as React from 'react';
+
+import DeleteTodoModal from '../modal-template/DeleteTodoModal';
+
+function ArticleEditDeleteDropdown({
+  title = '',
+  className = 'w-[16px] h-[16px]',
+  handleDelete,
+}: {
+  title?: string;
+  className?: string;
+  handleDelete: () => void;
+}) {
+  const [open, setOpen] = React.useState<boolean>(false);
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] =
+    React.useState<boolean>(false);
+
+  return (
+    <>
+      <DropdownMenu open={open} onOpenChange={setOpen}>
+        <DropdownMenuTrigger asChild>
+          <button
+            type="button"
+            aria-label="수정 및 삭제 기능 제공 드롭다운"
+            className="outline-none"
+          >
+            <KebabIcon className={`${className} hover:fill-text-tertiary`} />
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          align="end"
+          onClick={(e: React.MouseEvent<HTMLElement>) => {
+            e.stopPropagation();
+          }}
+        >
+          <DropdownMenuItem
+            className="flex cursor-pointer flex-col justify-center"
+            onClick={() => {
+              setOpen(false);
+            }}
+          >
+            수정하기
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="flex cursor-pointer flex-col justify-center"
+            onClick={() => {
+              setIsDeleteDialogOpen(true);
+              setOpen(false);
+            }}
+          >
+            삭제하기
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      {isDeleteDialogOpen && (
+        <DeleteTodoModal
+          type="article"
+          title={title}
+          onClick={handleDelete}
+          onClose={() => setIsDeleteDialogOpen(false)}
+        />
+      )}
+    </>
+  );
+}
+
+export default ArticleEditDeleteDropdown;
