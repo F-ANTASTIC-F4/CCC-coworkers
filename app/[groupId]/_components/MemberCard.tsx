576ec1d76e58353f7ebc0c92bb63b5bfd23b1e10
kimhyesun <hyesun1004.kim@gmail.com>
2024-08-28 16:41:53 +0900

    ♻️ refactor: 멤버 리스트 접속 상태 위치 옮기기 (Jira Ticket : CCC-91)

--- app/[groupId]/_components/MemberCard.tsx
+++ app/[groupId]/_components/MemberCard.tsx
@@ -50,13 +50,19 @@ function MemberCard({
         <div className="flex gap-2">
           <DefaultProfile className="size-6 md:size-8" />
           <div className="flex flex-col">
-            <p className="text-sm font-medium">{member.userName}</p>
+            <div className="flex items-center gap-2">
+              <p className="text-sm font-medium">{member.userName}</p>
+              <StateBullet
+                width={15}
+                height={15}
+                fill={userOnlineState ? '#10B981' : '#7c838e'}
+              />
+            </div>
             <p className="hidden truncate text-xs text-text-secondary md:block">
               {member.userEmail}
             </p>
           </div>
         </div>
-        <StateBullet fill={userOnlineState ? '#10B981' : '#7c838e'} />
       </div>
       <p className="truncate text-xs text-text-secondary md:hidden">
         {member.userEmail}
